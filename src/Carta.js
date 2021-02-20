2f2d45ac93ac5cc42d2f8ff3c0a8f469b1864015
sebaTruisi <sebatruisi@gmail.com>
2021-02-20 17:18:51 -0300

    Agregando imagenes

--- src/Carta.js
+++ src/Carta.js
@@ -1,17 +1,18 @@
 import React, { Component } from 'react';
 import './Carta.css';
 import FlipCard from 'react-card-flip';
+import imagen from './utils/Pokebolal.png'
 
 export default class Carta extends Component{ 
         
     render(){             
         return(
             <div className='carta' onClick = {this.props.seleccionarCarta}>
-                <FlipCard 
-                isFlipped={this.props.estaSiendoComparada || this.props.fueAdivinada}>
-                    <div className='front'></div>
+                <FlipCard isFlipped={this.props.estaSiendoComparada || this.props.fueAdivinada}>
+                    <div className='front'><img src={imagen} width='125px'/></div>
                     <div className='back'>
-                      <i className={`fa ${this.props.icono} fa-5x`}></i>
+                    <img className='icono' src='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/20.svg'></img>    
+                      {/* <i className={`fa ${this.props.icono} fa-5x`}></i> */}
                     </div>
                 </FlipCard>                
             </div>
